abd735b2e7f86a49597e11bc6a8dc89e47c8ebd8
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-03-20 20:59:24 +0530

    Add files via upload

---
+++ src/main/HibernateOneToOneBi/TestJdbc.java
@@ -0,0 +1,21 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		String jdbcurl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+		try {
+			Connection myconn = DriverManager.getConnection(jdbcurl, user, pass);
+			System.out.println("Connect success"+myconn);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
