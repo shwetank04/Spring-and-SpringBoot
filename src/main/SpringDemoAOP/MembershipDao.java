786d514437275c876b36dea3889b5959f5589b32
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-12-18 00:48:22 +0530

    Create MembershipDao.java

---
+++ src/main/SpringDemoAOP/MembershipDao.java
@@ -0,0 +1,12 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDao {
+	
+	public void addAccount() {
+		System.out.println("Fom membershipDao");
+	}
+
+}
